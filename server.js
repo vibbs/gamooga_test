f912f7378bd5bb5724abf396014bbea241fc34db
Vaibhav Doddihal <vibbsdod@Vaibhavs-MacBook-Pro.local>
2018-12-04 01:01:53 +0530

    client =server side code done

--- server.js
+++ server.js
@@ -11,18 +11,21 @@ app.get('/', function(req, res,next) {
 server.listen(4200);  
 
 
+
+var client_connections = {};
+
 var final_statement = "";
 var global_word_context = "";
-function emit_reverse(char){
+function emit_reverse(char, client_object){
     if(char === " " || char == undefined){
         var return_word = ""
-        for(var i =global_word_context.length-1 ; i >= 0 ;  i--){
-            final_statement += global_word_context[i];
+        for(var i =client_object.global_word_context.length-1 ; i >= 0 ;  i--){
+            client_object.final_statement += client_object.global_word_context[i];
         }
-        global_word_context = "";
-        final_statement += " "+ return_word;
+        client_object.global_word_context = "";
+        client_object.final_statement += " "+ return_word;
     }else{
-        global_word_context +=char
+        client_object.global_word_context +=char
     }
 }
 
@@ -32,20 +35,27 @@ io.on('connection', function(client) {
 
     client.on('join', function(connection_id) {
         console.log(connection_id);
+
+        if(!client_connections.hasOwnProperty(connection_id)){
+            client_connections[connection_id] = {
+                final_statement : "",
+                global_word_context : "",
+            };
+        }
+      
+        client.on(connection_id, function(char) {
+             if(char ==  "terminate-x-x-x"){
+                emit_reverse(" ", client_connections[connection_id])
+                client.emit(connection_id, client_connections[connection_id].final_statement);
+                client_connections[connection_id].final_statement = "";
+                client_connections[connection_id].global_word_context = "";
+             }else{
+                 emit_reverse(char, client_connections[connection_id])
+                 client.emit(connection_id, client_connections[connection_id].final_statement);
+             }
+         });
     })
 
-    client.on('messages', function(char) {
-       console.log(char);
-        if(char ==  "terminate-x-x-x"){
-            final_statement = "";
-            global_word_context = "";
-        }else if(char.length>1){
-            emit_reverse(" ")
-            client.emit('broad', final_statement);
-        }else{
-            emit_reverse(char)
-            client.emit('broad', final_statement);
-        }
-    });
+    
     
 });
